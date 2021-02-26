cf67e6d76d1a437f67d0d2351e2c3271777624f6
Lynne Cure <pjh.cure@gmail.com>
2021-02-25 23:11:20 -0500

    added out directory and fixed filenae input

--- split.py
+++ split.py
@@ -8,7 +8,7 @@ logging.basicConfig(format="%(levelname)s: %(message)s")
 argv = argv[1:]
 
 try:
-    filename = argv[0]
+    filename = argv[0].split("/")[-1]
 except IndexError:
     logging.error("No filename supplied")
     exit()
@@ -19,9 +19,7 @@ except:
     logging.info("Bad (or no) ratio given; defaulting to 5x3")
     columns, rows = (5, 3)
 
-# if filename.split(".")[-1].upper() == "gif":
-
-im = Image.open(filename)
+im = Image.open(argv[0])
 
 folder_name = "".join(filename.split(".")[:-1])
 
@@ -29,7 +27,6 @@ example = (50, 50)
 
 try:
     mkdir(folder_name)
-    mkdir(folder_name + "/raw")
 except:
     pass
 
@@ -51,8 +48,8 @@ for i,cell in enumerate(cells):
 
     if len(cells[i][0]) == 1:
         logging.warn("Only found 1 image; saving anyway")
-        cells[i][0][0].save(folder_name + "/" + folder_name + str(cell[1]) + ".gif")
+        cells[i][0][0].save("out/" + folder_name + "/" + folder_name + str(cell[1]) + ".gif")
     else:
-        cells[i][0][0].save(folder_name + "/" + folder_name + str(cell[1]) + ".gif", save_all = True, duration = 100, append_images = cells[i][0][1:])
+        cells[i][0][0].save("out/" + folder_name + "/" + folder_name + str(cell[1]) + ".gif", save_all = True, duration = 100, append_images = cells[i][0][1:])
 
     logging.info(f"Finished frame {i}")
